65933a04c1709e07173ee818514e311104cf7f0e
Stefan Dobrev <stefandobrev88@gmail.com>
2025-03-05 20:55:29 +0200

    first validation test passed, also included object deletion after every test for isaolation purposes

--- backend/api/tests/test_user_controller.py
+++ backend/api/tests/test_user_controller.py
@@ -5,12 +5,14 @@ from rest_framework.request import Request
 from rest_framework.parsers import JSONParser  
 from api.models import User
 from api.controllers.user_controller import UserController
+from django.urls import reverse
 
 @pytest.mark.django_db
 class TestUserController:
     def setup_method(self):
         self.controller = UserController()
         self.factory = APIRequestFactory()
+        User.objects.all().delete()
 
     def test_create_user_success(self):
         user_data = {
@@ -21,9 +23,11 @@ class TestUserController:
             "first_name": "test",
             "last_name": "user",
         }
+
+        url = reverse("create-user")
         
         django_request = self.factory.post(
-            "/user/create-user/", 
+            url,
             data=user_data, 
             format='json'
         )
@@ -40,3 +44,31 @@ class TestUserController:
         assert response.data["user"]["username"] == "testuser"
         assert response.data["user"]["email"] == "test@email.com"
         assert User.objects.filter(username="testuser").exists()
+        assert User.objects.count() == 1
+
+    def test_create_user_validation_error(self):
+        print(User.objects.count())
+        invalid_data = {
+            "username": "",
+            "email": "invalid_email",
+            "password": "securepassword123",
+            "confirm_password": "securepassword123",
+            "first_name": "test",
+            "last_name": "user",
+        }
+
+        url = reverse("create-user")
+        
+        django_request = self.factory.post(
+            url,
+            data=invalid_data, 
+            format='json'
+        )
+        drf_request = Request(
+            django_request, 
+            parsers=[JSONParser()]  
+        )
+
+        response = self.controller.create(drf_request)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert User.objects.count() == 0
--- backend/conftest.py
+++ backend/conftest.py
@@ -5,15 +5,15 @@ from django.conf import settings
 import pytest
 
 # Configure Django settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = "backend.settings"
 django.setup()
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def django_db_setup():
     """
     Customize database setup for tests
     """
-    settings.DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:'
+    settings.DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
     }
