89119e503aa87ac3affcec3ada576672a6497ef2
Stefan Dobrev <stefandobrev88@gmail.com>
2025-04-03 10:54:36 +0300

    npm updated and test_get_training_setup_data corrected

--- backend/api/tests/test_training_program_controller.py
+++ backend/api/tests/test_training_program_controller.py
@@ -6,7 +6,7 @@ from api.models import MuscleGroup, Exercise
 
 @pytest.mark.django_db(transaction=True)
 class TestTrainingProgramController:
-    def test_get_training_setup_data(self, api_client, test_admin, test_muscle_group):
+    def test_get_training_setup_data(self, api_client, test_user, test_admin, test_muscle_group):
         api_client.force_authenticate(user=test_admin)
         url = reverse("training-setup-data")
         response = api_client.get(url)
@@ -15,4 +15,5 @@ class TestTrainingProgramController:
 
         response_data = response.json()
         
-        assert test_muscle_group.slug in response_data
+        assert test_muscle_group.slug in response_data["muscle_groups"] 
+        assert any(user["username"] == test_user.username for user in response_data["users"])
