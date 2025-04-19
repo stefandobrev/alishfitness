a284be0e463a073be534fbd3e4ffd84b67611156
Stefan Dobrev <stefandobrev88@gmail.com>
2025-04-19 15:08:36 +0300

    Inline models transformed the admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,43 +4,46 @@ from api.models import User, MuscleGroup, Exercise, Step, Mistake, TrainingProgr
 class UserAdmin(admin.ModelAdmin):
     ordering = ["username"]
     list_display = ["username", "first_name", "last_name", "is_active"]
+    search_fields = ["username", "first_name", "last_name"]
 
 class MuscleGroupAdmin(admin.ModelAdmin):
     ordering = ["name"]
     list_display = ["name", "slug"]
 
+class StepInline(admin.TabularInline):
+    model = Step
+    extra = 0 
+    fields = ["order", "description"]  
+
+class MistakeInline(admin.TabularInline):
+    model = Mistake
+    extra = 0
+    fields = ["description"]
+
 class ExerciseAdmin(admin.ModelAdmin):
     ordering = ["title"]
     list_display = ["title", "primary_group", "id"]
+    search_fields = ["title", "primary_group"]
+    inlines = [StepInline, MistakeInline]
 
-class StepAdmin(admin.ModelAdmin):
-    ordering = ["exercise__title", "order"]
-    list_display = ["exercise", "order", "description"]
+class ProgramExerciseInline(admin.TabularInline):
+    model = ProgramExercise
+    extra = 0
+    fields = ["exercise", "muscle_group", "custom_exercise", "sequence", "sets", "reps"]
 
-class MistakeAdmin(admin.ModelAdmin):
-    ordering = ["exercise__title"]
-    list_display = ["exercise", "description"]
+class TrainingSessionInline(admin.TabularInline):
+    model = TrainingSession
+    extra = 0
+    fields = ["session_title"]
+    inlines = [ProgramExerciseInline]
 
 class TrainingProgramAdmin(admin.ModelAdmin):
     ordering = ["program_title"]
     list_display = ["mode", "program_title", "assigned_user", "activation_date"]
-
-class TrainingSessionAdmin(admin.ModelAdmin):
-    ordering = ["program__program_title"]
-    list_display = ["program", "session_title"]
-
-class ProgramExerciseAdmin(admin.ModelAdmin):
-    ordering = ["session__program__program_title", "session__session_title", "sequence"]
-    list_display = ["program_title", "session", "sequence", "exercise", "custom_exercise"]
-
-    def program_title(self, obj):
-        return obj.session.program.program_title
+    search_fields = ["program_title", "assigned_user"]
+    inlines = [TrainingSessionInline]
 
 admin.site.register(User, UserAdmin) 
 admin.site.register(MuscleGroup, MuscleGroupAdmin)
 admin.site.register(Exercise, ExerciseAdmin)
-admin.site.register(Step, StepAdmin)
-admin.site.register(Mistake, MistakeAdmin)
 admin.site.register(TrainingProgram, TrainingProgramAdmin)
-admin.site.register(TrainingSession, TrainingSessionAdmin)
-admin.site.register(ProgramExercise, ProgramExerciseAdmin)
