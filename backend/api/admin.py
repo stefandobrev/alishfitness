a69d805110ebcb057f069f118b09d9de4513a6d1
Stefan Dobrev <stefandobrev88@gmail.com>
2025-04-19 14:41:54 +0300

    admin.py includes new models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -21,9 +21,26 @@ class MistakeAdmin(admin.ModelAdmin):
     ordering = ["exercise__title"]
     list_display = ["exercise", "description"]
 
+class TrainingProgramAdmin(admin.ModelAdmin):
+    ordering = ["program_title"]
+    list_display = ["mode", "program_title", "assigned_user", "activation_date"]
+
+class TrainingSessionAdmin(admin.ModelAdmin):
+    ordering = ["program__program_title"]
+    list_display = ["program", "session_title"]
+
+class ProgramExerciseAdmin(admin.ModelAdmin):
+    ordering = ["session__program__program_title", "session__session_title", "sequence"]
+    list_display = ["program_title", "session", "sequence", "exercise", "custom_exercise"]
+
+    def program_title(self, obj):
+        return obj.session.program.program_title
 
 admin.site.register(User, UserAdmin) 
 admin.site.register(MuscleGroup, MuscleGroupAdmin)
 admin.site.register(Exercise, ExerciseAdmin)
 admin.site.register(Step, StepAdmin)
 admin.site.register(Mistake, MistakeAdmin)
+admin.site.register(TrainingProgram, TrainingProgramAdmin)
+admin.site.register(TrainingSession, TrainingSessionAdmin)
+admin.site.register(ProgramExercise, ProgramExerciseAdmin)
