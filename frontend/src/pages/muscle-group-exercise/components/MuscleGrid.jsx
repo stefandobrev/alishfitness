7687d2aa8568921d4e3dccc710d2232b011a9cc9
Stefan Dobrev <stefandobrev88@gmail.com>
2025-05-12 11:20:53 +0300

    added key to muscleGrid

--- frontend/src/pages/muscle-group-exercise/components/MuscleGrid.jsx
+++ frontend/src/pages/muscle-group-exercise/components/MuscleGrid.jsx
@@ -3,8 +3,8 @@ import { MuscleTile } from './MuscleTile';
 export const MuscleGrid = ({ exercisesData }) => {
   return (
     <div className='grid grid-cols-1 gap-2 px-3 md:grid-cols-2 lg:grid-cols-3'>
-      {exercisesData?.map((exercise) => (
-        <MuscleTile exercise={exercise} />
+      {exercisesData?.map((exercise, i) => (
+        <MuscleTile key={i} exercise={exercise} />
       ))}
     </div>
   );
