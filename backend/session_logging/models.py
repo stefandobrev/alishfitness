37a5b6f7ed3c79d6e785fdd509f41a0bf6620a91
Stefan Dobrev <stefandobrev88@gmail.com>
2025-06-04 16:27:48 +0300

    session_logging models added

--- backend/session_logging/models.py
+++ backend/session_logging/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from training_program.models import TrainingProgram, TrainingSession, TrainingExercise
 
-# Create your models here.
+class SessionLog(models.Model):
+    STATUS_CHOICES = [
+        ("in_progress", "In Progress"),
+        ("completed", "Completed"),
+    ]
+
+    training_program = models.ForeignKey(TrainingProgram, on_delete=models.CASCADE, db_index=True)
+    session = models.ForeignKey(TrainingSession, on_delete=models.CASCADE, db_index=True)
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="in_progress")
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+class SetLog(models.Model):
+    session_log = models.ForeignKey(SessionLog, on_delete=models.CASCADE, related_name="set_logs", db_index=True)
+    exercise = models.ForeignKey(TrainingExercise, on_delete=models.CASCADE, db_index=True)
+    weight = models.PositiveIntegerField(null=True, blank=True)
+    reps = models.PositiveIntegerField(null=True, blank=True)
