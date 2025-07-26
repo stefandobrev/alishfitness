e232fe24d0de178295075b5cc497ea707567e0a9
Stefan Dobrev <stefandobrev88@gmail.com>
2025-07-26 18:47:12 +0300

    migration to postgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,12 +83,23 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#         "USER": "your_db_user",
+#         "PASSWORD" :  "your_db_password" ,
+#         "HOST" :  "localhost" ,
+#         "PORT" :  "5432" ,
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        "USER": "your_db_user",
-        "PASSWORD" :  "your_db_password" ,
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "gymdb",
+        "USER": "postgres",
+        "PASSWORD" :  "Graheca26@" ,
         "HOST" :  "localhost" ,
         "PORT" :  "5432" ,
     }
